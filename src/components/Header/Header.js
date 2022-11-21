0eaab052dfb6f61e73cf0c0d9c4069a8a2a4c20b
luc-lenovo <ksluccio@gmail.com>
2022-11-21 19:40:08 +0100

    Text decoration Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,14 @@ import { Link, useLocation } from 'react-router-dom';
 
 function Header() {
   let { pathname } = useLocation();
+  // const currentMobile = (pathname.includes("propos") && pathname !== "/propos") || (!pathname.includes("propos")) ? "current" : undefined ; +++ MOBILE BREAKPOINTS CONDITIONS
 
   return (
     <header className="Header">
       <img className="Header__logo" src="/images/logo.svg" alt="logo Kasa" />
       <ul className="Header__navbar">
         <li className="Header__navbar-element">
-          <Link to="/" className={ (pathname.includes("propos") && pathname !== "/propos") || (!pathname.includes("propos")) ? "current" : undefined}>Accueil</Link>
+          <Link to="/" className={ pathname === "/" ? "current" : undefined }>Accueil</Link>
         </li>
         <li className="Header__navbar-element">
           <Link to="/propos" className={ pathname === "/propos" ? "current" : undefined }>A Propos</Link>
